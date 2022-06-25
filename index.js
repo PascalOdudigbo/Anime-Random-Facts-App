194e1ad82892d3ff5dc68d676ce4a9b91e9671f7
Pascal Odudigbo <odudigbopascal@gmail.com>
2022-06-25 13:09:39 +0300

    modified my fetch random facts function to handle all aspects of fetching, randomizing and presenting facts display, i also styled the facts presentation

--- index.js
+++ index.js
@@ -1,70 +1,68 @@
-document.addEventListener("DOMContentLoaded", ()=>{
-    fetchAnimeNames();
-    dropDownClickEvent();
-   
+document.addEventListener("DOMContentLoaded", () => {
+	fetchAnimeNames();
+	dropDownClickEvent();
 
-    
 
-})
 
-function fetchAnimeNames(){
-    const dropdownElements = document.getElementById("myDropdown");
-    fetch("https://anime-facts-rest-api.herokuapp.com/api/v1")
-    .then(response => response.json())
-    .then(data => {
-        //console.log(data);
-        //console.log(data.data);
-        data.data.forEach(anime => {
-            //console.log(anime.anime_name);
-            const btnSelectAnime = document.createElement("button");
-            const animeNAme = anime.anime_name;
-            btnSelectAnime.innerText = animeNAme.replaceAll('_', ' ');
-            console.log(anime);
-            btnSelectAnime.addEventListener("click", () =>{
-                //console.log(anime);
-                const dropdownElements = document.getElementById("myDropdown");
-                const animeNAmeElement = document.getElementById("name");
-                const animeImage = document.getElementById("animeImage");
-                animeNAmeElement.textContent = anime.anime_name.replaceAll('_', ' ').toUpperCase();
-                animeImage.setAttribute("src", anime.anime_img);
-                dropdownElements.style.display = "none";
-                fetchRandomFacts(anime.anime_name);
-            
-            });
-            dropdownElements.appendChild(btnSelectAnime);
-            
-            
-        })
-    })
+
+});
+
+function fetchAnimeNames() {
+	const dropdownElements = document.getElementById("myDropdown");
+	fetch("https://anime-facts-rest-api.herokuapp.com/api/v1")
+		.then(response => response.json())
+		.then(data => {
+			data.data.forEach(anime => {               
+				const btnSelectAnime = document.createElement("button");
+				const animeNAme = anime.anime_name;
+				btnSelectAnime.innerText = animeNAme.replaceAll('_', ' ');
+				btnSelectAnime.addEventListener("click", () => {
+                    fetchRandomFacts(anime.anime_name);
+					const animeNAmeElement = document.getElementById("name");
+					const animeImage = document.getElementById("animeImage");
+					animeNAmeElement.textContent = anime.anime_name.replaceAll('_', ' ').toUpperCase();
+					animeImage.setAttribute("src", anime.anime_img);
+					dropdownElements.style.display = "none";
+                    
+				});
+				dropdownElements.appendChild(btnSelectAnime);
+
+
+			});
+		});
 }
 
-function dropDownClickEvent(){
-    const btnDropdown = document.getElementById("dropbtn");
-    const dropdownElements = document.getElementById("myDropdown");
-    btnDropdown.addEventListener("click", ()=>{
-        if(dropdownElements.style.display === "none"){
-            dropdownElements.style.display = "block";
-        }
-        else{
-            dropdownElements.style.display = "none";
-        }
-        
-    });
+function dropDownClickEvent() {
+	const btnDropdown = document.getElementById("dropbtn");
+	const dropdownElements = document.getElementById("myDropdown");
+	btnDropdown.addEventListener("click", () => {
+		if (dropdownElements.style.display === "none") {
+			dropdownElements.style.display = "block";
+		} else {
+			dropdownElements.style.display = "none";
+		}
+
+	});
 }
 
-function fetchRandomFacts(animeName){
-    const factsArray = [];
-    fetch(`https://anime-facts-rest-api.herokuapp.com/api/v1/${animeName}`)
-    .then(response => response.json())
-    .then(data => {
-        //console.log(data)
-        //console.log(data.data)
-        data.data.forEach(animeFact =>{
-            factsArray.push(animeFact.fact);
-        })
-    .catch(err => setTimeout(alert(err.message), 3000))
+function fetchRandomFacts(animeName) {
+    const randomFactElement = document.getElementById("RandomFact");
+	const factsArray = new Array;
+	fetch(`https://anime-facts-rest-api.herokuapp.com/api/v1/${animeName}`)
+		.then(response => response.json())
+		.then(data => {
+			data.data.forEach(animeFact => {
+				factsArray.push(animeFact.fact);
 
-    })
-    return factsArray;
+			});
+            setInterval(()=>{
+                const randomIndex = (Math.floor(Math.random() * factsArray.length));
+			    let randomFact = factsArray[randomIndex];
+	            randomFactElement.innerText = randomFact;
+            }, 5000)
+            
+		})
+		.catch(err => setTimeout(alert(err.message), 3000));
+
+
 }
-
